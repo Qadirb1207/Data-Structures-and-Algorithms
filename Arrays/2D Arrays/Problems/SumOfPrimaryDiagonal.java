1988a7de8738b38b5b322c4ebc941d0ffed8efbc
Qadir <qadirb3492@gmail.com>
2026-06-05 15:33:57 +0500

    Added a program to find the sum of diagonals of matrices

---
+++ Arrays/2D Arrays/Problems/SumOfPrimaryDiagonal.java
@@ -0,0 +1,25 @@
+package Two_Dim_Array.Problems;
+
+public class SumOfPrimaryDiagonal {
+    public static int primaryDiagonalSum(int[][] arr){
+        int diagonalSum = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(i==j){
+                    diagonalSum += arr[i][j];
+                }
+            }
+        }
+        return diagonalSum;
+    }
+    public static void main(String[] args) {
+        int[][] arr = {
+                        {10, 40, 70},
+                        {34, 21, 19},
+                        {20, 80, 26}
+                        };
+        
+        System.out.println("Sum of Diagonal Elements of the array is: "+primaryDiagonalSum(arr));
+        
+    }
+}
