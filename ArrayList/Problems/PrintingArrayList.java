35a8d88a70bba80ee479a14f30f37b8a78a47a2b
Qadir <qadirb3492@gmail.com>
2026-06-05 16:13:36 +0500

    Implemented a program to print an ArrayList
    Implemented a program to first take an ArrayList input from the user and then printing it

---
+++ ArrayList/Problems/PrintingArrayList.java
@@ -0,0 +1,27 @@
+package Problems;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintingArrayList {
+    public static void printArraylist(ArrayList<Integer> list){
+        for (Integer integer : list) {
+            System.out.print(integer+"\t");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("Enter the size of array list = ");
+        int size = sc.nextInt();
+
+
+
+        for(int i = 0; i < size; i++){
+            System.out.print("Enter the element no: "+(i+1)+ " = ");
+            list.add(sc.nextInt());
+        }
+        printArraylist(list);
+    }
+}
