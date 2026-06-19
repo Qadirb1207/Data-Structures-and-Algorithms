957e638108ec2b30452572e12b7d891067dcee9a
Qadir <qadirb3492@gmail.com>
2026-06-19 06:06:51 +0500

    Implemented a program to find the last occurrence of an element
    Implemented a program to find the last occurrence of an ArrayList element

---
+++ ArrayList/1D ArrayLists/Algorithms/LastOccurrenceOfElement.java
@@ -0,0 +1,17 @@
+public class LastOccurenceOfElement{
+  public static void main(String[] args){
+    
+  }
+  
+  public static int lastOccurrence(ArrayList<Integer> list, int target){
+    int start = 0, end = list.size()-1;
+    while(start <= end){
+      int mid = start+(end-start)/2;
+      
+      if(list.get(mid) == target){
+      
+      }
+      
+    }
+  }
+}
