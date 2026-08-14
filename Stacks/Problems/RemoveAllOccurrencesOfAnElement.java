b97850c9f3437046b98c64de21bc575ac7e44c0d
Qadir <qadirb3492@gmail.com>
2026-08-14 16:35:05 +0500

    Refactor removeAll to remove specified occurrences
    Refactor removeAll method to count and remove occurrences of a specified number from the data array, while maintaining the order of other elements.

--- Stacks/Problems/RemoveAllOccurrencesOfAnElement.java
+++ Stacks/Problems/RemoveAllOccurrencesOfAnElement.java
@@ -37,24 +37,29 @@ class Stack{
     this.index = -1;
   }
   public void removeAll(int n){
+    //counting occurrences of the number to be removed
     int count = 0;
     for(int i = 0; i < data.length; i++){
       if(data[i] == n){
         ++count;
       }
     }
+    //array for storting all the elements other than n
     int[] arr = new int[data.length-count];
-    int arrIndex = arr.length-1; 
+    int arrIndex = arr.length-1; //variable for taking the index of arr
+    
     for(int i = data.length-1; i >= 0 ; i--){
-      if(data[i] != n){
+      if(data[i] != n){//putting the element in the arr if it is not equal to n
         arr[arrIndex] = this.pop();
         --arrIndex;
       }else{
+        //else just drop the number
         index -= 1;
       }
     }
-    this.clear();
-    this.data = new int[arr.length];
+    this.clear();//making the member variable index -1
+    this.data = new int[arr.length];//assigning a new object to the data array of the size same as arr
+    //pushing the elements of the array in the data
     for(int i = 0; i < arr.length; i++){
       push(arr[i]);
     }
